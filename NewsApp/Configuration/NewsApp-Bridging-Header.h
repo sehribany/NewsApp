13135de5cf964b15d355198a63cb636b352656dc
Şehriban Yıldırım <sehribanyildirim051@gmail.com>
2023-12-13 14:12:39 +0300

    Created LaunchScreen

---
+++ NewsApp/Configuration/NewsApp-Bridging-Header.h
@@ -0,0 +1,10 @@
+//
+//  NewsApp-Bridging-Header.h
+//  NewsApp
+//
+//  Created by Şehriban Yıldırım on 10.12.2023.
+//
+
+#import <Utilities/Utilities.h>
+#import <UIComponents/UIComponents.h>
+#import <DataProvider/DataProvider.h>
