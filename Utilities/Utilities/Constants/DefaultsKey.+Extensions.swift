05b1134e3720aeaa15c7432d6ff98f54aafca19e
Şehriban Yıldırım <sehribanyildirim051@gmail.com>
2023-12-13 14:08:04 +0300

    Created Constants

---
+++ Utilities/Utilities/Constants/DefaultsKey.+Extensions.swift
@@ -0,0 +1,12 @@
+//
+//  DefaultsKey+Extensions.swift
+//  Utilities
+//
+//  Created by Şehriban Yıldırım on 10.12.2023.
+//
+import MobilliumUserDefaults
+
+public extension DefaultsKey{
+    static let isIntroCompleted = Key<Bool>(key: "isIntroCompleted")
+    static let userId           = Key<Int>(key: "userId")
+}
---
+++ Utilities/Utilities/Constants/Keychain+Constants.swift
@@ -0,0 +1,10 @@
+//
+//  Keychain+Constants.swift
+//  Utilities
+//
+//  Created by Şehriban Yıldırım on 10.12.2023.
+//
+
+public struct Keychain {
+    public static let token = "token"
+}
