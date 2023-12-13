4e581edf29a55c460f231364fff97f88dce0ef9b
Şehriban Yıldırım <sehribanyildirim051@gmail.com>
2023-12-13 13:59:32 +0300

    Created Closures

---
+++ Utilities/Utilities/Closures/Closures.swift
@@ -0,0 +1,12 @@
+//
+//  Closures.swift
+//  Utilities
+//
+//  Created by Şehriban Yıldırım on 10.12.2023.
+//
+
+public typealias VoidClosure      = (() -> Void)
+public typealias IntClosure       = ((Int) -> Void)
+public typealias BoolClosure      = ((Bool) -> Void)
+public typealias StringClosure    = ((String) -> Void)
+public typealias IndexPathClosure = ((IndexPath) -> Void)
