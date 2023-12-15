2f3faaf7a138927d694b28bb9f30262bcafc25c1
Şehriban Yıldırım <sehribanyildirim051@gmail.com>
2023-12-15 16:34:45 +0300

    Added ReusableView

---
+++ UIComponents/UIComponents/Protocols/ReusableView.swift
@@ -0,0 +1,10 @@
+//
+//  ReusableView.swift
+//  UIComponents
+//
+//  Created by Şehriban Yıldırım on 10.12.2023.
+//
+
+public protocol ReusableView: AnyObject {
+    static var identifier: String { get }
+}
