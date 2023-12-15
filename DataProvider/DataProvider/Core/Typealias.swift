a608c69f04ae9c1b035851b8fdf726eb5cc951de
Şehriban Yıldırım <sehribanyildirim051@gmail.com>
2023-12-15 16:30:10 +0300

    Added Typealias

---
+++ DataProvider/DataProvider/Core/Typealias.swift
@@ -0,0 +1,9 @@
+//
+//  Typealias.swift
+//  DataProvider
+//
+//  Created by Şehriban Yıldırım on 10.12.2023.
+//
+
+public typealias RequestParameters = [String: Any]
+public typealias RequestHeaders    = [String: String]
