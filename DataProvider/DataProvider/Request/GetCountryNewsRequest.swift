9155f0bb33a9fa1cafdfed952cb1ed9fa2e39e20
Şehriban Yıldırım <sehribanyildirim051@gmail.com>
2023-12-17 23:54:34 +0300

    Added GetCountryNewsRequest

--- DataProvider/DataProvider/Request/GetCountryNewsRequest.swift
+++ DataProvider/DataProvider/Request/GetCountryNewsRequest.swift
@@ -5,4 +5,20 @@
 //  Created by Şehriban Yıldırım on 16.12.2023.
 //
 
-import Foundation
+public struct GetCountryNewsRequest: APIDecodableResponseRequest{
+    
+    public typealias ResponseType = News
+    
+    public var path       : String            = ""
+    public var method     : RequestMethod     = .get
+    public var parameters : RequestParameters = [:]
+    public var headers    : RequestHeaders    = [:]
+
+    public init(page: Int){
+        self.parameters["page"] = page
+        self.path = "?country=us&"
+    }
+}
+
+
+//https://newsapi.org/v2/top-headlines  ?country=us&  apiKey=aa76d1da00604d86a1f9cc53cc425ed0
