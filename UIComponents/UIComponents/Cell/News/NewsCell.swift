7de67865af135d9d33c052ef5065224c447c100a
Şehriban Yıldırım <sehribanyildirim051@gmail.com>
2023-12-19 00:18:16 +0300

    News cell is complete.

--- UIComponents/UIComponents/Cell/News/NewsCell.swift
+++ UIComponents/UIComponents/Cell/News/NewsCell.swift
@@ -12,6 +12,8 @@ public class NewsCell: UICollectionViewCell, ReusableView{
     
     private let newsCard = NewsCardView()
     
+    private let lineView = UIView()
+    
     weak var viewModel: NewsCellProtocol?
     
     override init(frame: CGRect) {
@@ -26,10 +28,15 @@ public class NewsCell: UICollectionViewCell, ReusableView{
     
     private func configureContents() {
         addSubview(newsCard)
-        newsCard.backgroundColor = .appWhite
+        newsCard.backgroundColor    = .appWhite
+        newsCard.layer.cornerRadius = 2
+        
+        let bottomBorder = CALayer()
+        let borderWidth: CGFloat = 0.3
+        bottomBorder.frame = CGRect(x: 20, y: bounds.size.height - borderWidth, width: bounds.size.width - 40, height: borderWidth)
+        bottomBorder.backgroundColor = Asset.Colors.appTitle.color.cgColor
+        layer.addSublayer(bottomBorder)
         newsCard.edgesToSuperview(insets: .init(top: 0, left: 10, bottom: 0, right: 10))
-        //newsCard.edgesToSuperview()
-        newsCard.layer.cornerRadius = 40
     }
     
     public func set(viewModel: NewsCellProtocol) {
