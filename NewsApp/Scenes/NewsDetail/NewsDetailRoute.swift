de6dfdcaaad3a8a47bfe58189a0321e23e12f533
Şehriban Yıldırım <sehribanyildirim051@gmail.com>
2023-12-19 23:05:15 +0300

    Added NewsDetailRoute

--- NewsApp/Scenes/NewsDetail/NewsDetailRoute.swift
+++ NewsApp/Scenes/NewsDetail/NewsDetailRoute.swift
@@ -6,3 +6,20 @@
 //
 
 import Foundation
+
+protocol NewsDetailRoute{
+    func pushNewsDetail(article: Article)
+}
+
+extension NewsDetailRoute  where Self: RouterProtocol {
+    func pushNewsDetail(article: Article){
+        let router            = NewsDetailRouter()
+        let viewModel         = NewsDetailViewModel(articleDetail: article, router: router)
+        let viewController    = NewsDetailViewController(viewModel: viewModel)
+        
+        let transition        = PushTransition()
+        router.viewController = viewController
+        router.openTransition = transition
+        open(viewController, transition: transition)
+    }
+}
