f5ce370554b971d886e16fa103e21c55234f16ef
Şehriban Yıldırım <sehribanyildirim051@gmail.com>
2023-12-19 23:04:59 +0300

    Added NewsDetailViewModel

--- NewsApp/Scenes/NewsDetail/NewsDetailViewModel.swift
+++ NewsApp/Scenes/NewsDetail/NewsDetailViewModel.swift
@@ -5,14 +5,22 @@
 //  Created by Şehriban Yıldırım on 19.12.2023.
 //
 
-import Foundation
+import DataProvider
 
-protocol NewsDetailViewDataSource {}
+protocol NewsDetailViewDataSource {
+    var articleDetail : Article { get}
+}
 
 protocol NewsDetailViewEventSource {}
 
 protocol NewsDetailViewProtocol: NewsDetailViewDataSource, NewsDetailViewEventSource {}
 
 final class NewsDetailViewModel: BaseViewModel<NewsDetailRouter>, NewsDetailViewProtocol {
+    var articleDetail    : Article
     
+    init(articleDetail: Article, router: NewsDetailRouter) {
+        self.articleDetail = articleDetail
+        super.init(router: router)
+    }
 }
+
