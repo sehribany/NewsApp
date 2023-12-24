48f79ea4ec5f3a8569a9d769835094db0d1a3cbd
Şehriban Yıldırım <sehribanyildirim051@gmail.com>
2023-12-24 21:40:29 +0300

    update HeaderViewModel

--- NewsApp/Scenes/Categories/Header/HeaderViewModel.swift
+++ NewsApp/Scenes/Categories/Header/HeaderViewModel.swift
@@ -5,4 +5,35 @@
 //  Created by Şehriban Yıldırım on 24.12.2023.
 //
 
-import Foundation
+protocol HeaderViewDataSource {
+    func numberOfItemsAt(section: Int) -> Int
+    func cellItemAt(indexPath: IndexPath) -> HeaderViewCellProtocol
+}
+
+protocol HeaderViewEventSource {
+    func didSelectHeaderItem(at index: IndexPath)
+}
+
+protocol HeaderViewProtocol: HeaderViewDataSource, HeaderViewEventSource {}
+
+final class HeaderViewModel: HeaderViewProtocol {
+
+    private var cellCategories : [HeaderViewCellProtocol] = [HeaderViewCellModel(titleText: "Business"),
+                                                             HeaderViewCellModel(titleText: "Entertainment"),
+                                                             HeaderViewCellModel(titleText: "Health"),
+                                                             HeaderViewCellModel(titleText: "Science"),
+                                                             HeaderViewCellModel(titleText: "Sports"),
+                                                             HeaderViewCellModel(titleText: "Technology")]
+    
+    func numberOfItemsAt(section: Int) -> Int {
+        cellCategories.count
+    }
+    
+    func cellItemAt(indexPath: IndexPath) -> HeaderViewCellProtocol {
+        cellCategories[indexPath.row]
+    }
+    
+    func didSelectHeaderItem(at index: IndexPath) {
+        print(index)
+    }
+}
